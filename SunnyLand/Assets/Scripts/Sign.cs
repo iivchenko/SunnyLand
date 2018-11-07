04250f6480d3d900f32d5510219beb72bcf18a83
iivchenko <iivchenko@live.com>
2018-11-07 22:19:09 +0200

    Create a primitive sign prefab

---
+++ SunnyLand/Assets/Scripts/Sign.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public sealed class Sign : MonoBehaviour
+{
+    private MeshRenderer _text;
+
+    public void Awake()
+    {
+        _text = GetComponentsInChildren<MeshRenderer>().First();
+    }
+
+    public void Start()
+    {
+        _text.enabled = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _text.enabled = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _text.enabled = false;
+        }
+    }
+}
