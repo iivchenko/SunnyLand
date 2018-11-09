d110b605f57499aca6af96e1c64d1384c3bf89c8
iivchenko <iivchenko@live.com>
2018-11-09 21:54:35 +0200

    Rework kill zone

---
+++ SunnyLand/Assets/Scripts/Levels/Level 3/DeathDoorTriggerHandler.cs
@@ -0,0 +1,8 @@
+﻿public class DeathDoorTriggerHandler : TriggerHandler
+{
+    public override void Handle(TriggerEventArgs args)
+    {
+        gameObject.gameObject.SetActive(true);
+    }
+}
+
---
+++ SunnyLand/Assets/Scripts/Levels/PlayerReachedDeathZoneHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+
+public class PlayerReachedDeathZoneHandler : TriggerHandler
+{  
+    public override void Handle(TriggerEventArgs args)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
