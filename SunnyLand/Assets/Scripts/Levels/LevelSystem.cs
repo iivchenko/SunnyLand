017e89b00d3708a2cd4d9f1bd73e0f7871929eb0
iivchenko <iivchenko@live.com>
2018-11-09 21:53:19 +0200

    Move LevelSystem

---
+++ SunnyLand/Assets/Scripts/Levels/LevelSystem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSystem : MonoBehaviour
+{
+    public string _nextScene;
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
