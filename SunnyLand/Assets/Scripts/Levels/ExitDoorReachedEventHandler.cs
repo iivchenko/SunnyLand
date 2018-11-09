6e3bd359c30649c0175f0b47213dd88509f14521
iivchenko <iivchenko@live.com>
2018-11-09 21:55:11 +0200

    Rework Exit

---
+++ SunnyLand/Assets/Scripts/Levels/ExitDoorReachedEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using UnityEngine.SceneManagement;
+
+public sealed class ExitDoorReachedEventHandler : TriggerHandler
+{
+    public string[] _actors;
+
+    private LevelSystem _system;
+
+    public void Awake()
+    {
+        _system = GetComponent<LevelSystem>() as LevelSystem;
+    }
+
+    public override void Handle(TriggerEventArgs args)
+    {
+        if (_actors.Contains(args.Other.tag))
+        {
+            SceneManager.LoadScene(_system._nextScene);
+        }
+    }
+}
