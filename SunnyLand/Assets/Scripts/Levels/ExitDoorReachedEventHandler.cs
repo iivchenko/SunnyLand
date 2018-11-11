27a8a2e4f35ad0d12c5d4f44925fbd321dc5daf9
iivchenko <iivchenko@live.com>
2018-11-11 23:25:32 +0200

    Extend Exit Door to play sound

--- SunnyLand/Assets/Scripts/Levels/ExitDoorReachedEventHandler.cs
+++ SunnyLand/Assets/Scripts/Levels/ExitDoorReachedEventHandler.cs
@@ -1,22 +1,41 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public sealed class ExitDoorReachedEventHandler : TriggerHandler
 {
     public string[] _actors;
-
+    public AudioClip _victoryAudio;
+    private AudioSource _sound;
     private LevelSystem _system;
 
     public void Awake()
     {
-        _system = GetComponent<LevelSystem>() as LevelSystem;
+        _system = GetComponent<LevelSystem>();
+        _sound = GetComponent<AudioSource>();
     }
 
     public override void Handle(TriggerEventArgs args)
     {
         if (_actors.Contains(args.Other.tag))
         {
-            SceneManager.LoadScene(_system._nextScene);
+            // TODO: Stop player from mooving
+            StartCoroutine("PlayVictory");
         }
+    }
+
+    private IEnumerator PlayVictory()
+    {
+        _sound.Stop();
+        _sound.PlayOneShot(_victoryAudio);
+
+        while (_sound.isPlaying)
+        {
+            yield return null;
+        }
+
+        SceneManager.LoadScene(_system._nextScene);
     }
 }
